b31b4770c61bcd29f93296aae2f6ba5d830aa5e7
Susmitadash07 <dashsusmita999@gmail.com>
2023-10-19 23:38:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="logo-box">
+        <img src="./twitter.png" alt="logo" className="logo"></img>
+        <h2>Sign In To Twitter </h2>
+        <button>
+        <img src="./google.png" alt="logo"></img>
+        Sign In With Google
+        </button>
+        <button>
+        <img src="./apple.png" alt="logo"></img>
+        Sign In With Apple
+        </button>
+        <hr></hr>
+        <span>Or</span>
+        <form>
+          <input type="text" placeholder="Phone Number,Email Or UserName"></input>
+          <button>Next</button>
+        </form>
+        <button>Forget Password</button>
+        <p>Don't Have An Account? <a>Sign Up</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
